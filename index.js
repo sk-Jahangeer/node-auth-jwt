cdf680bb4fe611b1bbdbf8d66461e3125926c910
sk-jahangeer <jahangeer.webdev@gmail.com>
2021-06-07 17:52:31 +0530

    added cors

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ require("dotenv").config();
 const users = require("./routes/users");
 const auth = require("./routes/auth");
 const connection = require("./db");
+const cors = require("cors");
 const express = require("express");
 const app = express();
 
 connection();
+
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/users", users);
 app.use("/api/auth", auth);
